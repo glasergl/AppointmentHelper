51a7616959140543bd40d589a1a0659eb3081979
Gabriel Glaser <gabriel.glaser@web.de>
2022-03-18 14:32:03 +0100

    rename output files

--- build-script.bat
+++ build-script.bat
@@ -1,12 +1,12 @@
 @echo off
 
 echo delete old files
-del AppointmentAdapter.jar
-del AppointmentCalendar.jar
-del AppointmentEditor.jar
-del AppointmentReminder.jar
+del TerminAdapter.jar
+del TerminKalender.jar
+del TerminEditor.jar
+del TerminReminder.jar
 
-del AppointmentHelper.zip
+del TerminHelfer.zip
 
 del appointments.json
 
@@ -30,13 +30,13 @@ echo ################################################################-Reminder-#
 call mvn package -f Reminder/
 
 echo rename jars
-rename "AppointmentAdapter-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "AppointmentAdapter.jar"
-rename "AppointmentCalendar-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "AppointmentCalendar.jar"
-rename "AppointmentEditor-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "AppointmentEditor.jar"
-rename "AppointmentReminder-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "AppointmentReminder.jar"
+rename "AppointmentAdapter-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminAdapter.jar"
+rename "AppointmentCalendar-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminKalender.jar"
+rename "AppointmentEditor-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminEditor.jar"
+rename "AppointmentReminder-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminReminder.jar"
 
 echo pack jars in zip:
-call 7z a AppointmentHelper.zip AppointmentAdapter.jar AppointmentCalendar.jar AppointmentEditor.jar AppointmentReminder.jar
+call 7z a TerminHelfer.zip TerminAdapter.jar TerminKalender.jar TerminEditor.jar TerminReminder.jar
 
 echo\
 
