c269c9c046a3ba446e851bdf50c09b6db6b377e2
Gabriel Glaser <gabriel.glaser@web.de>
2022-03-25 01:42:23 +0100

    create output directory

--- build-script.bat
+++ build-script.bat
@@ -1,14 +1,10 @@
 @echo off
 
 echo delete old files
-del TerminAdapter.jar
-del TerminKalender.jar
-del TerminEditor.jar
-del TerminReminder.jar
+del /s /q output\*.*
+echo\
 
-del TerminHelfer.zip
-
-del appointments.json
+echo build modules
 
 echo ################################################################-Base-################################################################
 call mvn clean install -f Base/
@@ -28,16 +24,13 @@ call mvn clean package -f Editor/
 echo ################################################################-Reminder-################################################################
 call mvn clean package -f Reminder/
 
-echo rename jars
-rename "AppointmentAdapter-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminAdapter.jar"
-rename "AppointmentCalendar-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminKalender.jar"
-rename "AppointmentEditor-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminEditor.jar"
-rename "AppointmentReminder-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "TerminReminder.jar"
-
-echo pack jars in zip:
-call 7z a TerminHelfer.zip TerminAdapter.jar TerminKalender.jar TerminEditor.jar TerminReminder.jar
-
 echo\
+echo create Geburtstage.txt
+echo Test1.21.9>>output\Geburtstage.txt
+echo Test2.27.8>>output\Geburtstage.txt
+echo Test3.25.9>>output\Geburtstage.txt
+echo Test4.5.5>>output\Geburtstage.txt
+echo Test5.25.2>>output\Geburtstage.txt
 
 echo build finished.
 
