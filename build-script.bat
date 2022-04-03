e1ba1c0c7d5808a71a514e3b1b2be41c2d5e9bfa
Gabriel Glaser <gabriel.glaser@web.de>
2022-04-03 04:48:25 +0200

    adapt to restructure

--- build-script.bat
+++ build-script.bat
@@ -1,39 +1,32 @@
 @echo off
 
-echo delete old files
+echo delete old output
 del /s /q output\*.*
+
 echo\
-
 echo build modules
-
-echo ################################################################-Base-################################################################
-call mvn clean install -f Base/
-
-echo ################################################################-FileInteraction-################################################################
-call mvn clean install -f FileInteraction/
-
-echo ################################################################-Errors-################################################################
-call mvn clean install -f Errors/
-
-echo ################################################################-Adapter-################################################################
-call mvn clean package -f Adapter/
-
-echo ################################################################-Calendar-################################################################
-call mvn clean install -f Calendar/
-
-echo ################################################################-Editor-################################################################
-call mvn clean package -f Editor/
-
-echo ################################################################-Reminder-################################################################
-call mvn clean package -f Reminder/
+echo - Base:
+call mvn clean install -f source/Base/
+echo - FileInteraction:
+call mvn clean install -f source/FileInteraction/
+echo - Errors:
+call mvn clean install -f source/Errors/
+echo - Calendar:
+call mvn clean install -f source/Calendar/
+echo - Adapter:
+call mvn clean package -f source/Adapter/
+echo - Editor:
+call mvn clean package -f source/Editor/
+echo - Reminder:
+call mvn clean package -f source/Reminder/
 
 echo\
 echo create .exe-Files
-call launch4jc Calendar\launch4j_configuration.xml
+call launch4jc source\Calendar\launch4j_configuration.xml
 echo\
-call launch4jc Editor\launch4j_configuration.xml
+call launch4jc source\Editor\launch4j_configuration.xml
 echo\
-call launch4jc Reminder\launch4j_configuration.xml
+call launch4jc source\Reminder\launch4j_configuration.xml
 
 echo\
 echo create Geburtstage.txt
@@ -44,14 +37,14 @@ echo Test4.5.5>>output\Geburtstage.txt
 echo Test5ßöäü.25.2>>output\Geburtstage.txt
 
 echo\
-echo pack .exe-Files in zip
-call 7z a output\TerminHelfer.zip .\output\*.exe
-
-echo\
 echo adapt "Geburtstage.txt" to "appointments.json"
-call cd output
+cd output
 rename "AppointmentAdapter-0.0.1-SNAPSHOT-jar-with-dependencies.jar" "AppointmentAdapter.jar"
 call java -jar AppointmentAdapter.jar
+
+echo\
+echo pack .exe-Files in zip
+call 7z a TerminHelfer.zip *.exe
 
 echo\
 echo build finished.
