347ec441cd0aec3bf5c26259e456c818c31c815a
Gabriel Glaser <gabriel.glaser@web.de>
2022-04-04 21:30:13 +0200

    remove calendar from reminder

--- source/Reminder/src/main/java/ui/ReminderFrame.java
+++ source/Reminder/src/main/java/ui/ReminderFrame.java
@@ -2,16 +2,12 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Image;
 import java.util.List;
 import appointment.Appointment;
 import appointmentMessage.TodayTomorrowAppointmentMessagePanel;
-import calendar.CalendarPanel;
-import standardSwing.container.JPanelFactory;
 import standardSwing.container.MyFrame;
 import standardSwing.general.SwingFunctions;
-import standardSwing.myComponent.button.MyJButton;
 import standardSwing.settings.Colors;
 
 /**
@@ -26,14 +22,11 @@ public final class ReminderFrame extends MyFrame {
     private static final Image ICON = SwingFunctions.getImage("ReminderIcon.png", ReminderFrame.class);
     private static final Color BACKGROUND_COLOR_OF_APPOINTMENT_MESSAGES = Colors.getBlue(0);
 
-    private final MyJButton calendarVisabilityChangerButton = new MyJButton("Kalender anzeigen");
     private final TodayTomorrowAppointmentMessagePanel appointmentMessages;
-    private final CalendarPanel calendarWithAppointments;
 
     public ReminderFrame(final List<Appointment> allAppointments) {
 	super(FRAME_NAME, ICON);
 	this.appointmentMessages = new TodayTomorrowAppointmentMessagePanel(allAppointments);
-	this.calendarWithAppointments = new CalendarPanel(allAppointments);
 	setup();
 	if (anyIsTodayOrTomorrow(allAppointments)) {
 	    start();
@@ -41,21 +34,8 @@ public final class ReminderFrame extends MyFrame {
     }
 
     private void setup() {
-	setupCalendarVisabilityButton();
 	appointmentMessages.setBackground(BACKGROUND_COLOR_OF_APPOINTMENT_MESSAGES);
-	calendarWithAppointments.setVisible(false);
-	add(appointmentMessages, BorderLayout.NORTH);
-	add(JPanelFactory.create(new FlowLayout(FlowLayout.RIGHT, 0, 0), Colors.getBlue(0), calendarVisabilityChangerButton), BorderLayout.CENTER);
-	add(calendarWithAppointments, BorderLayout.SOUTH);
-    }
-
-    private void setupCalendarVisabilityButton() {
-	calendarVisabilityChangerButton.addActionListener(click -> {
-	    calendarWithAppointments.setVisible(!calendarWithAppointments.isVisible());
-	    calendarVisabilityChangerButton.setText(calendarWithAppointments.isVisible() ? "Kalender verbergen" : "Kalender anzeigen");
-	    pack();
-	    setLocationRelativeTo(null);
-	});
+	add(appointmentMessages, BorderLayout.CENTER);
     }
 
     private boolean anyIsTodayOrTomorrow(final List<Appointment> appointmentsToTest) {
