0d2543c829cbbf2237879cd9a2dfd2e3ab74c507
Gabriel Glaser <gabriel.glaser@web.de>
2024-01-07 16:49:14 +0100

    remove unused imports

--- AppointmentHelper-Utilities/src/main/java/de/glasergl/fileConfiguration/ConfigurationHandler.java
+++ AppointmentHelper-Utilities/src/main/java/de/glasergl/fileConfiguration/ConfigurationHandler.java
@@ -8,8 +8,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
