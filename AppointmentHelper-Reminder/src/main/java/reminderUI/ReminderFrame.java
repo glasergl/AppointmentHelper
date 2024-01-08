657850ba65a38b493fbc486390ab2f6600bd835f
Gabriel Glaser <gabriel.glaser@web.de>
2024-01-08 04:10:02 +0100

    remove custom background and simplify as a result

--- AppointmentHelper-Reminder/src/main/java/reminderUI/ReminderFrame.java
+++ AppointmentHelper-Reminder/src/main/java/reminderUI/ReminderFrame.java
@@ -1,7 +1,6 @@
 package reminderUI;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Image;
 import java.util.List;
 
@@ -9,7 +8,6 @@ import javax.swing.JFrame;
 
 import appointment.Appointment;
 import de.glasergl.standard.swing.general.SwingFunctions;
-import de.glasergl.standard.swing.settings.Colors;
 
 /**
  * Main Frame for the Reminder.
@@ -17,10 +15,8 @@ import de.glasergl.standard.swing.settings.Colors;
  * @author Gabriel Glaser
  */
 public final class ReminderFrame extends JFrame {
-
 	private static final String FRAME_NAME = "TerminReminder";
 	private static final Image ICON = SwingFunctions.getImage("ReminderIcon.png", ReminderFrame.class);
-	private static final Color BACKGROUND_COLOR_OF_APPOINTMENT_MESSAGES = Colors.getBlue(0);
 
 	private final TodayTomorrowAppointmentMessagePanel appointmentMessages;
 
@@ -29,17 +25,12 @@ public final class ReminderFrame extends JFrame {
 		setIconImage(ICON);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.appointmentMessages = new TodayTomorrowAppointmentMessagePanel(allAppointments);
-		setup();
+		add(appointmentMessages, BorderLayout.CENTER);
 		if (anyIsTodayOrTomorrow(allAppointments)) {
 			pack();
 			setLocationRelativeTo(null);
 			setVisible(true);
 		}
-	}
-
-	private void setup() {
-		appointmentMessages.setBackground(BACKGROUND_COLOR_OF_APPOINTMENT_MESSAGES);
-		add(appointmentMessages, BorderLayout.CENTER);
 	}
 
 	private boolean anyIsTodayOrTomorrow(final List<Appointment> appointmentsToTest) {
@@ -50,5 +41,4 @@ public final class ReminderFrame extends JFrame {
 		}
 		return false;
 	}
-
 }
